aa09408b0c7462d006330d61370400fb5f5aa4f0
Serge Var <serge@pointnetwork.io>
2021-09-21 16:00:21 +0300

    Activate nvm

--- install-debian.sh
+++ install-debian.sh
@@ -5,14 +5,22 @@
 # wget -qO- pointer.sh | wget
 
 echo "Point Network Installer installing..."
+
 sudo apt install git nodejs -y || exit 1
+
+# nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-. ~/.bashrc || exit 1
+. ~/.bashrc # this doesn't work (https://stackoverflow.com/questions/43659084/source-bashrc-in-a-script-not-working), so:
+### the following is from what nvm installs into .bashrc:
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 DASHBOARDDIR=$HOME/.point/dashboard
 mkdir -p $DASHBOARDDIR
 git clone https://github.com/pointnetwork/pointnetwork-dashboard $DASHBOARDDIR || echo "Already exists." # no exit because if it already exists, just update from git pull below
 cd $DASHBOARDDIR || exit 1
 git pull || exit 1 # update in case it's there but of sync
+
 nvm install || exit 1
 nvm use || exit 1
 npm install || exit 1
