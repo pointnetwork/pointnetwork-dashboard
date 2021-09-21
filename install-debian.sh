0f8bd62d6a21924a1f171e104da669d9e6f2b0ee
Serge Var <serge@pointnetwork.io>
2021-09-21 15:49:10 +0300

    Dont die when git repo already exists

--- install-debian.sh
+++ install-debian.sh
@@ -10,8 +10,9 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 . ~/.bashrc || exit 1
 DASHBOARDDIR=$HOME/.point/dashboard
 mkdir -p $DASHBOARDDIR
-git clone https://github.com/pointnetwork/pointnetwork-dashboard $DASHBOARDDIR || exit 1
+git clone https://github.com/pointnetwork/pointnetwork-dashboard $DASHBOARDDIR # no exit because if it already exists, just update from git pull below
 cd $DASHBOARDDIR || exit 1
+git pull || exit 1 # update in case it's there but of sync
 nvm install || exit 1
 nvm use || exit 1
 npm install || exit 1
