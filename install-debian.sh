7ea8e7949a2b8b7578e6aec9dc686042327db921
Serge Var <serge@pointnetwork.io>
2021-09-21 15:44:38 +0300

    Add info message and select home dir

--- install-debian.sh
+++ install-debian.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+echo "Point Network Installer installing..."
 sudo apt install git nodejs -y || exit 1
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc || exit 1
-git clone https://github.com/pointnetwork/pointnetwork-dashboard || exit 1
-cd pointnetwork-dashboard || exit 1
+DASHBOARDDIR=$HOME/.point/dashboard
+mkdir -p $DASHBOARDDIR
+git clone https://github.com/pointnetwork/pointnetwork-dashboard $DASHBOARDDIR || exit 1
+cd $DASHBOARDDIR || exit 1
 nvm install || exit 1
 nvm use || exit 1
 npm install || exit 1
