9a226f342856a3a552588f045bea536448c9ac8e
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-09-21 20:58:41 -0700

    Initial code for Mac-Docker installation.

--- src/docker/index.js
+++ src/docker/index.js
@@ -12,5 +12,51 @@ module.exports = {
             return `wsl ${cmd}`;
         }
         return cmd;
-    }
+    },
+
+    getFileName(osAndArch, version) {
+        if (osAndArch == 'win32' || osAndArch == 'win64') {
+            return 'Docker Desktop Installer.exe';
+        }
+        if (osAndArch == 'mac') {
+            return `Docker.dmg`;
+        }
+        // linux & mac
+        return '';
+    },
+
+    unpack(osAndArch, releasePath, browserDir, cb) {
+        if (osAndArch == 'win32' || osAndArch == 'win64') {
+
+        }
+        if (osAndArch == 'mac') {
+            dmg.mount(releasePath, (err, dmgPath) => {
+                fs.copy(`${dmgPath}/Docker.app`, `${browserDir}/Docker.app`, (err) => {
+                    if (err) {
+                        console.log("Error Found:", err);
+                        dmg.unmount(dmgPath, (err) => { if (err) throw err;});
+                        return;
+                    }
+                    dmg.unmount(dmgPath, (err) => { if (err) throw err; cb();});
+                });
+            });
+            return;
+        }
+        if (osAndArch == 'linux-x86_64' || osAndArch == 'linux-i686') {
+
+        }
+    },
+
+    getURL(version, osAndArch, language, filename) {
+        if (osAndArch == 'win32' || osAndArch == 'win64') {
+            return 'https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?utm_source=docker&utm_medium=webreferral&utm_campaign=dd-smartbutton&utm_location=module';
+        }
+        if (osAndArch == 'mac') {
+            return 'https://desktop.docker.com/mac/main/amd64/Docker.dmg?utm_source=docker&amp;utm_medium=webreferral&amp;utm_campaign=dd-smartbutton&amp;utm_location=module';
+        }
+        if (osAndArch == 'linux') {
+            return '';
+        }
+        throw "unrecognized platform";
+    },
 };
