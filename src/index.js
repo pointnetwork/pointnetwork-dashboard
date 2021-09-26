b33a8f56dc81e67e59bcd7d16c2ef12dcb0d3181
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-09-26 00:17:53 -0700

    Uncommenting lines that start installer.

--- src/index.js
+++ src/index.js
@@ -69,6 +69,12 @@ app.whenReady().then(() => {
         return;
     }
 
+    if (! isLoggedIn()) {
+        const welcome = new Welcome();
+        welcome.run();
+        return;
+    }
+
     createWindow();
 
     app.on('activate', function () {
