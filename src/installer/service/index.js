a9b29dae42950ab634cc75b47cb120a4962823fe
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-09-26 00:05:11 -0700

    Added steps for installing WSL and running docker compose once installer is done.

--- src/installer/service/index.js
+++ src/installer/service/index.js
@@ -19,13 +19,21 @@ class InstallerService {
     constructor(win) {
         this.win = win;
         this.osAndArch = getOSAndArch();
-        this.steps = {
-            // todo: install wsl
+        
+        this.steps = {};
+        if (this.isWindows()) {
+            this.steps = {
+                'Install WSL': this.installWSL,
+            };
+        }
+        
+        Object.assign(this.steps, {
             'Create ~/.point directory': this.makePointDirectory,
             'Create ~/.point/src directory': this.makePointSrcDirectory,
             'Clone PointNetwork repositories': this.cloneRepos,
             'Install Docker': this.installDocker,
-        };
+            'Running Docker Containers': this.runDockerCompose,
+        });
     }
 
     async start() {
@@ -63,8 +71,14 @@ class InstallerService {
         fsExtra.mkdirpSync(this.pointSrcDir);
     }
 
-    async installWSL() {
-        
+    async installWSL(testRun = false) {
+        if (testRun) {
+            if (which.sync('wsl', {nothrow: true}) != null) {
+                return true;
+            }
+            return false;
+        }
+        sudo.exec('wsl --install');
     }
 
     async installDocker(testRun = false) {
@@ -101,6 +115,22 @@ class InstallerService {
         sudo.exec('curl -L '+this._quote("https://github.com/docker/compose/releases/download/"+latest_tag+"/docker-compose-$(uname -s)-$(uname -m)")+' -o /usr/local/bin/docker-compose');
     }
 
+    // TODO: Create `helpers.getDockerComposePath` and use that.
+    // TODO: Move `this._exec` to `helpers` or something.
+    // TODO: Just noticed `../welcome` has exact copies of _exec. Refactor before doing above TODOs.
+    async runDockerCompose(testRun = false) {
+        // TODO: `this._execAndGetOutput` doesn't return output, so it can't be used to test.
+        // if (testRun) return await this._execAndGetOutput('docker-compose ps -q');
+        if (testRun) return false;
+        const pnPath = await helpers.getPNPath(this.osAndArch);
+        const composePath = helpers.fixPath(this.osAndArch, path.join(pnPath, 'docker-compose.yaml'));
+        // TODO: `docker-compose.dev.yaml` is buggy at the moment.
+        // const composeDevPath = helpers.fixPath(osAndArch, path.join(pnPath, 'docker-compose.dev.yaml'));
+        // const cmd = `docker-compose -f ${composePath} -f ${composeDevPath} up -d`;
+        const cmd = `docker-compose -f ${composePath} up -d`;
+        await this._exec(cmd);
+    }
+
     async cloneRepos(testRun = false) {
         await this._clonePointNetworkRepo(testRun, 'pointnetwork');
     }
@@ -122,11 +152,11 @@ class InstallerService {
     }
 
     isWindows() {
-        return isWindows64() || isWindows32();
+        return this.isWindows64() || this.isWindows32();
     }
 
     isWindows10Pro() {
-        return isWindows() && os.release().indexOf("Pro");
+        return this.isWindows() && os.release().indexOf("Pro");
     }
 
     _log(text, opts) {
