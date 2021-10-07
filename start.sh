0796b69390eb6105726153847cc39f8264c4f3fe
Serge Var <serge@pointnetwork.io>
2021-10-07 19:53:24 +0300

    Improvements

--- start.sh
+++ start.sh
@@ -59,7 +59,7 @@ git pull
 cd $HOME/.point/src/pointnetwork-dashboard || exit 1
 git pull
 nvm use
-npm i > /dev/null 2>&1
+npm i
 
 #if is_linux; then
 #echo "Point Dashboard needs sudo in order to access docker containers on Linux. Please enter below, or Ctrl-C for exit:"
