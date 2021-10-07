bb6f168d00534643d8caf88fd01db593ee91a497
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-10-06 19:24:42 -0700

    Sourcing nvm and adding shebang bash.

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+try_source_nvm() {
+    if [[ -f $HOME/.nvm/nvm.sh ]]; then
+	    source $HOME/.nvm/nvm.sh
+    fi
+    if [[ -f $HOME/.bashrc ]]; then
+	    source $HOME/.bashrc
+    fi
+}
+
+try_source_nvm
+
 #!/usr/bin/env bash
 cd $HOME/.point/src/pointnetwork || exit 1
 git pull
