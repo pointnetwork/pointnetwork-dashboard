85fa04647995effc863768d3b0240a8365a47614
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-10-10 15:07:31 -0700

    Improved Windows installation script

--- install-windows.ps1
+++ install-windows.ps1
@@ -6,6 +6,14 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 }
 
 $CMDS = @('choco', 'git', 'wget', 'curl', 'wsl', 'nvm', 'node', 'docker', 'docker-compose')
+$POINT_DIR="$HOME\.point"
+$SRC_DIR="$POINT_DIR\src"
+$SRC_PN_DIR="$SRC_DIR\pointnetwork"
+$SRC_DASHBOARD_DIR="$SRC_DIR\pointnetwork-dashboard"
+$SRC_SDK_DIR="$SRC_DIR\pointsdk"
+$SOFTWARE_DIR="$POINT_DIR\software"
+$LIVE_DIR="$POINT_DIR\live"
+$NODE_VERSION="v14.18.0"
 
 function Msg($msg) {
     Write-Host ">>> " -ForegroundColor Red -NoNewline
@@ -31,21 +39,28 @@ function Test-WSLInstalled() {
     return $(If (Get-Command wslconfig) {$true} Else {$false})
 }
 
-function Test-AllCommandsExist() {
-    foreach ($cmd in $CMDS) {
-	switch ($cmd)
-	{
-	    'wsl' {
-		return Test-WSLInstalled
-	    }
-	    default {
-		if(-Not(Get-Command $cmd)) {
-		    Write-Host("Exists $true")
-		    return $false
-		}
+function Test-Command($cmd) {
+    switch ($cmd)
+    {
+	'wsl' {
+	    if(-Not(Test-WSLInstalled)) {
+		return $false
 	    }
 	}
-	
+	default {
+	    if(-Not(Get-Command $cmd)) {
+		return $false
+	    }
+	}
+    }
+    return $true
+}
+
+function Test-AllCommandsExist() {
+    foreach ($cmd in $CMDS) {
+	if(-Not(Test-Command($cmd))) {
+	    return $false
+	}
     }
     return $true
 }
@@ -63,15 +78,32 @@ function Install-WSL() {
     wsl --install
 }
 
+function Install-Docker() {
+    Msg("Installing Docker")
+    # Invoke-WebRequest "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe" -OutFile $SOFTWARE_DIR\Docker.exe
+    & $SOFTWARE_DIR/Docker.exe
+}
+
+function Remove-Docker() {
+    Remove-Item $SOFTWARE_DIR/Docker.exe
+}
+
 function Install-Commands() {
     foreach ($cmd in $CMDS) {
+	if (Test-Command($cmd)) {
+	    # Already installed. Continue.
+	    continue
+	}
 	switch ($cmd)
 	{
 	    'wsl' {
-		return Install-WSL
+		Install-WSL
 	    }
 	    'choco' {
-		return Install-Chocolatey
+		Install-Chocolatey
+	    }
+	    'docker' {
+		Install-Docker
 	    }
 	    default {
 		choco install $cmd
@@ -80,7 +112,125 @@ function Install-Commands() {
     }
 }
 
-function EchoWelcome {
+function Create-Directory($path) {
+    If(!(test-path $path)) {
+	Msg("Creating $path directory")
+	New-Item -ItemType Directory -Force -Path $path
+    }
+}
+
+function Create-Directories() {
+    Create-Directory($POINT_DIR)
+    Create-Directory($SRC_DIR)
+    Create-Directory($SRC_PN_DIR)
+    Create-Directory($SRC_DASHBOARD_DIR)
+    Create-Directory($SOFTWARE_DIR)
+    Create-Directory($LIVE_DIR)
+}
+
+function Test-DashboardRepo() {
+    If(test-path $SRC_DASHBOARD_DIR) {
+	return $true
+    }
+    return $false
+}
+
+function Test-PNRepo() {
+    If(test-path $SRC_PN_DIR) {
+	return $true
+    }
+    return $false
+}
+
+function Test-SDKRepo() {
+    If(test-path $SRC_SDK_DIR) {
+	return $true
+    }
+    return $false
+}
+
+function Test-AllRepos() {
+    return (Test-SDKRepo -And Test-PNRepo -And Test-DashboardRepo)
+}
+
+function Test-AllInstalled() {
+    if (Test-AllCommandsExist -And Test-AllRepos) {
+	Msg("")
+	Msg("Congratulations, you have all the necessary components to run PointNetwork!")
+	Msg("")
+	Msg("")
+    } else {
+	Msg("Something is wrong. Not all commands are installed. Please check the logs")
+	Exit 1
+    }
+}
+
+function Clone-Repository($repo) {
+    # git clone $repoLocation$gitRepo $localFolder\$gitRepo
+}
+
+function Clone-Repositories() {
+    if(!(Test-PNRepo)) {
+	Msg("Cloning PointNetwork")
+	git clone https://github.com/pointnetwork/pointnetwork $SRC_PN_DIR
+    }
+    if(!(Test-DashboardRepo)) {
+	Msg("Cloning PointNetwork Dashboard")
+	git clone https://github.com/pointnetwork/pointnetwork-dashboard $SRC_DASHBOARD_DIR
+    }
+    if(!(Test-SDKRepo)) {
+	Msg("Cloning PointSDK")
+	git clone https://github.com/pointnetwork/pointsdk $SRC_SDK_DIR
+    }
+}
+
+function Update-Repositories() {
+    if(Test-PNRepo) {
+	Msg("Updating PointNetwork")
+	git --git-dir=$SRC_PN_DIR/.git pull
+    }
+    if(Test-DashboardRepo) {
+	Msg("Updating PointNetwork Dashboard")
+	git --git-dir=$SRC_DASHBOARD_DIR/.git pull
+    }
+    if(Test-SDKRepo) {
+	Msg("Updating PointSDK")
+	git --git-dir=$SRC_SDK_DIR/.git pull
+    }
+}
+
+function Copy-BrowserProfile() {
+    If(!(test-path $LIVE_DIR/profile)) {
+	Msg("Copying browser profile")
+	Copy-Item -Path $SRC_DASHBOARD_DIR/liveprofile -Recurse -Destination $LIVE_DIR/profile -Container
+    }
+}
+
+function Run-Dashboard() {
+    cd $SRC_DASHBOARD_DIR
+    Msg("Installing required node.js version")
+    nvm install $NODE_VERSION | out-null
+    Msg("Changing to required node.js version")
+    nvm use | out-null
+    Msg("Installing required node.js packages using npm")
+    npm install | out-null
+    Msg("Starting PointNetwork Dashboard")
+    npm start
+}
+
+function Create-Shortcut() {
+    $startScript = "$SRC_DASHBOARD_DIR\start.ps1"
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $shortcutLocation = "$HOME/Desktop/Point.lnk"
+    $shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+    # $shortcut.IconLocation="$SRC_DASHBOARD_DIR\resources\pointlogo_any_bg.png"
+    $shortcut.IconLocation="$SRC_DASHBOARD_DIR\resources\pointlogo_any_bg.ico"
+    $shortcut.TargetPath = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
+    $shortcut.Arguments = "-command & `"$startScript`""
+    $shortcut.Save()
+}
+
+function Echo-Welcome {
     Msg("")
     Msg("Welcome to PointNetwork Installer")
     Msg("")
@@ -95,14 +245,18 @@ function EchoWelcome {
     Msg("")
 }
 
-EchoWelcome
-# Write-Host(Ask)
-# Test-AllCommandsExist
-# Test-WSLInstalled
-# Install-Chocolatey
+Echo-Welcome
 Install-Commands
-if (Ask "Do you want to continue?") {
-    Msg "Hell yeah"
-} else {
-    Msg "Hell no"
-}
+Create-Directories
+Clone-Repositories
+Copy-BrowserProfile
+Update-Repositories
+Test-AllInstalled
+Create-Shortcut
+# Create-Aliases # TODO
+Run-Dashboard
+# if (Ask "Do you want to continue?") {
+#     Msg "Yes"
+# } else {
+#     Msg "No"
+# }
