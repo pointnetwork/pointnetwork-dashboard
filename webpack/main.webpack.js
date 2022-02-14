f998b4bbe6b62c3bcf15bce8eb472966b115c373
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-02-14 17:07:58 +0530

    Update configuration to work with new changes

--- webpack/main.webpack.js
+++ webpack/main.webpack.js
@@ -2,7 +2,7 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
   },
-  entry: './src/main/main.ts',
+  entry: './src/main/index.ts',
   module: {
     rules: require('./rules.webpack'),
   },
