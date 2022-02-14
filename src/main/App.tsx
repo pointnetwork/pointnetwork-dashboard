c78254ae7283945b771946de3e8dd3fe22057a49
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-02-14 17:11:39 +0530

    Code clean up

--- src/main/App.tsx
+++
@@ -1,9 +0,0 @@
-import Installer from '../installer/ui'
-
-export function App() {
-  return (
-    <>
-      <Installer />
-    </>
-  )
-}
--- src/main/ui.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import './app.global.css';
-
-ReactDOM.render(<App />, document.getElementById("root"));
