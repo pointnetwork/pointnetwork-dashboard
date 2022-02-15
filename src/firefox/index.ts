36be832432bfb3174113d78d26ee88b875563701
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-02-15 18:04:44 +0530

    Update the helpers usage in files

--- src/firefox/index.ts
+++ src/firefox/index.ts
@@ -3,14 +3,14 @@ import path from 'path'
 import _7z from '7zip-min'
 import tarfs from 'tar-fs'
 import url from 'url'
-import { Helpers } from '../../shared/helpers'
+import helpers from '../../shared/helpers'
 import util from 'util'
 import https from 'follow-redirects'
 import { BrowserWindow } from 'electron'
 
-const dmg = require("dmg")
+const dmg = require('dmg')
 const bz2 = require('unbzip2-stream')
-const find = require('find-process');
+const find = require('find-process')
 const exec = util.promisify(require('child_process').exec)
 
 export default class {
@@ -19,16 +19,14 @@ export default class {
     this.window = window
   }
 
-  private helpers = new Helpers() 
+  private flagPath = 'installer-finished'
 
-  private flagPath = "installer-finished"
-    
-  async getFolderPath(osAndArch: any) {
-    return await this.helpers.getBrowserFolderPath(osAndArch)
+  async getFolderPath() {
+    return await helpers.getBrowserFolderPath()
   }
 
   async isInstalled() {
-    const osAndArch = this.helpers.getOSAndArch()
+    const osAndArch = helpers.getOSAndArch()
 
     const binPath = await this.getBinPath(osAndArch)
     if (fs.existsSync(binPath)) {
@@ -39,7 +37,7 @@ export default class {
 
   getURL(version: unknown, osAndArch: any, language: string, filename: string) {
     if (global.platform.win32) {
-      return "https://github.com/pointnetwork/phyrox-esr-portable/releases/download/test/point-browser-portable-win64-78.12.0-55.7z"
+      return 'https://github.com/pointnetwork/phyrox-esr-portable/releases/download/test/point-browser-portable-win64-78.12.0-55.7z'
     }
     // linux & mac
     return `https://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/${version}/${osAndArch}/${language}/${filename}`
@@ -62,12 +60,12 @@ export default class {
     console.log('download firefox')
     this.window.webContents.send('firefox:log', 'Installing Firefox...')
 
-    const language = "en-US"
+    const language = 'en-US'
     const version = await this.getLastVersionFirefox() // '93.0b4'//
-    const osAndArch = this.helpers.getOSAndArch()
-    const browserDir = await this.getFolderPath(osAndArch)
+    const osAndArch = helpers.getOSAndArch()
+    const browserDir = await this.getFolderPath()
     const pacFile = url.pathToFileURL(
-      path.join(await this.helpers.getPNPath(osAndArch), "client", "proxy", "pac.js")
+      path.join(await helpers.getPNPath(), 'client', 'proxy', 'pac.js')
     )
     const filename = this.getFileName(osAndArch, version)
     const releasePath = path.join(browserDir, filename)
@@ -78,63 +76,67 @@ export default class {
       fs.mkdirSync(browserDir)
     }
 
-    return https.https.get(firefoxURL, async (response: { pipe: (arg0: fs.WriteStream) => any }) => {
+    return https.https.get(
+      firefoxURL,
+      async (response: { pipe: (arg0: fs.WriteStream) => any }) => {
         await response.pipe(firefoxRelease)
 
         return await new Promise((resolve, reject) => {
-            firefoxRelease.on("finish", () => {
-                const cb = async () => {
-                    fs.unlink(releasePath, (err) => {
-                        if (err) {
-                            return reject(err)
-                        } else {
-                            console.log(`\nDeleted file: ${releasePath}`)
-                            this.window.webContents.send('firefox:log', 'Installed Successfully')
-                            this.launch()
-                            return resolve()
-                        }
-                    })
-
-                    await this.createConfigFiles(osAndArch, pacFile)
-
-                    
+          firefoxRelease.on('finish', () => {
+            const cb = async () => {
+              fs.unlink(releasePath, err => {
+                if (err) {
+                  return reject(err)
+                } else {
+                  console.log(`\nDeleted file: ${releasePath}`)
+                  this.window.webContents.send(
+                    'firefox:log',
+                    'Installed Successfully'
+                  )
+                  this.launch()
+                  return resolve()
                 }
-                this.unpack(osAndArch, releasePath, browserDir, cb)
-            })
+              })
+
+              await this.createConfigFiles(osAndArch, pacFile)
+            }
+            this.unpack(osAndArch, releasePath, browserDir, cb)
+          })
         })
-    })
+      }
+    )
   }
 
   async launch() {
     const isRunning = await find('name', 'Firefox')
-    if(isRunning.length > 0 ) {
+    if (isRunning.length > 0) {
       console.log('Firefox already Running')
       this.window.webContents.send('firefox:log', 'Firefox already Running')
       return
     }
 
-    const osAndArch = this.helpers.getOSAndArch()
+    const osAndArch = helpers.getOSAndArch()
     const cmd = await this.getBinPath(osAndArch)
     const profilePath = path.join(
-      this.helpers.getHomePath(),
-      ".point/keystore/profile"
+      helpers.getHomePath(),
+      '.point/keystore/profile'
     )
-    let webextBinary = ""
+    let webextBinary = ''
     if (global.platform.win32) {
-      webextBinary = "web-ext"
+      webextBinary = 'web-ext'
     } else {
       webextBinary = path.join(
-        this.helpers.getHomePath(),
-        ".point/src/pointnetwork-dashboard/node_modules/web-ext/bin/web-ext"
+        helpers.getHomePath(),
+        '.point/src/pointnetwork-dashboard/node_modules/web-ext/bin/web-ext'
       )
       // webextBinary = 'web-ext'
     }
-    // const webextBinary = path.join(await this.helpers.getHomePath(), ".point/src/pointnetwork-dashboard/node_modules/web-ext/bin/web-ext")
+    // const webextBinary = path.join(await helpers.getHomePath(), ".point/src/pointnetwork-dashboard/node_modules/web-ext/bin/web-ext")
     const extPath = path.join(
-      this.helpers.getHomePath(),
-      ".point/src/pointsdk/dist/prod"
+      helpers.getHomePath(),
+      '.point/src/pointsdk/dist/prod'
     ) // should contain manifest.json
-    let webext = ""
+    let webext = ''
     if (global.platform.win32) {
       webext = `"${webextBinary}" run "--firefox=${cmd}" "--firefox-profile=${profilePath}" --keep-profile-changes "--source-dir=${extPath}" --url https://point`
     } else {
@@ -149,38 +151,38 @@ export default class {
       }
       if (stderr) {
         console.log(`stderr: ${stderr}`)
-        
       }
     })
     this.window.webContents.send('firefox:log', 'Firefox Start')
   }
 
-  unpack(_osAndArch: any, releasePath: string, browserDir: string, cb: { (): Promise<void>; (): void }) {
+  unpack(
+    _osAndArch: any,
+    releasePath: string,
+    browserDir: string,
+    cb: { (): Promise<void>; (): void }
+  ) {
     if (global.platform.win32) {
-      _7z.unpack(releasePath, browserDir, (err) => {
+      _7z.unpack(releasePath, browserDir, err => {
         if (err) throw err
         cb()
       })
     }
     if (global.platform.darwin) {
       dmg.mount(releasePath, (_err: any, dmgPath: any) => {
-        fs.copy(
-          `${dmgPath}/Firefox.app`,
-          `${browserDir}/Firefox.app`,
-          (err) => {
-            if (err) {
-              console.log("Error Found:", err)
-              dmg.unmount(dmgPath, (err: any) => {
-                if (err) throw err
-              })
-              return
-            }
+        fs.copy(`${dmgPath}/Firefox.app`, `${browserDir}/Firefox.app`, err => {
+          if (err) {
+            console.log('Error Found:', err)
             dmg.unmount(dmgPath, (err: any) => {
               if (err) throw err
-              cb()
             })
+            return
           }
-        )
+          dmg.unmount(dmgPath, (err: any) => {
+            if (err) throw err
+            cb()
+          })
+        })
       })
       return
     }
@@ -190,27 +192,27 @@ export default class {
         .pipe(bz2())
         .pipe(tarfs.extract(browserDir))
       // readStream.on('finish', () => {cb()} )
-      readStream.on("finish", cb)
+      readStream.on('finish', cb)
     }
   }
 
   async getRootPath(osAndArch: any) {
     if (global.platform.win32 || global.platform.darwin) {
-      return path.join(await this.getFolderPath(osAndArch))
+      return path.join(await this.getFolderPath())
     }
     // linux
-    return path.join(await this.getFolderPath(osAndArch), "firefox")
+    return path.join(await this.getFolderPath(), 'firefox')
   }
 
   async getAppPath(osAndArch: any) {
     const rootPath = await this.getRootPath(osAndArch)
 
     if (global.platform.win32 || global.platform.darwin) {
-      let appPath = ""
+      let appPath = ''
       if (global.platform.darwin) {
-        appPath = path.join(rootPath, "Firefox.app", "Contents", "Resources")
+        appPath = path.join(rootPath, 'Firefox.app', 'Contents', 'Resources')
       } else {
-        appPath = path.join(rootPath, "app")
+        appPath = path.join(rootPath, 'app')
       }
 
       if (!fs.existsSync(appPath)) {
@@ -228,15 +230,15 @@ export default class {
     const rootPath = await this.getRootPath(osAndArch)
 
     if (global.platform.win32 || global.platform.darwin) {
-      let appPath = ""
+      let appPath = ''
       if (global.platform.darwin) {
-        appPath = path.join(rootPath, "Firefox.app", "Contents", "Resources")
+        appPath = path.join(rootPath, 'Firefox.app', 'Contents', 'Resources')
       } else {
-        appPath = path.join(rootPath, "app")
+        appPath = path.join(rootPath, 'app')
       }
 
-      const defaultsPath = path.join(appPath, "defaults")
-      const prefPath = path.join(defaultsPath, "pref")
+      const defaultsPath = path.join(appPath, 'defaults')
+      const prefPath = path.join(defaultsPath, 'pref')
 
       if (!fs.existsSync(appPath)) {
         fs.mkdirSync(appPath)
@@ -251,30 +253,30 @@ export default class {
       return prefPath
     }
     // linux. all directories already exist.
-    return path.join(rootPath, "defaults", "pref")
+    return path.join(rootPath, 'defaults', 'pref')
   }
 
   async getBinPath(osAndArch: string) {
     const rootPath = await this.getRootPath(osAndArch)
     if (global.platform.win32) {
-      return path.join(rootPath, "point-browser-portable.exe")
+      return path.join(rootPath, 'point-browser-portable.exe')
     }
     if (global.platform.darwin) {
-      return `${path.join(rootPath, "Firefox.app")}`
+      return `${path.join(rootPath, 'Firefox.app')}`
     }
     // linux
-    return path.join(rootPath, "firefox")
+    return path.join(rootPath, 'firefox')
   }
 
   async createConfigFiles(osAndArch: any, pacFile: url.URL) {
     if (!pacFile)
-      throw Error("pacFile sent to createConfigFiles is undefined or null!")
+      throw Error('pacFile sent to createConfigFiles is undefined or null!')
 
-    let networkProxyType = ""
+    let networkProxyType = ''
     if (global.platform.win32) {
-      networkProxyType = "1"
+      networkProxyType = '1'
     }
-    networkProxyType = "2"
+    networkProxyType = '2'
 
     const autoconfigContent = `pref("general.config.filename", "firefox.cfg")
 pref("general.config.obscure_value", 0)
@@ -314,12 +316,11 @@ pref('browser.tabs.drawInTitlebar', true)
       // Portapps also creates `portapps.cfg`, which is equivalent to *nix's firefox.cfg.
       // We're just appending our preferences.
       fs.appendFile(
-        path.join(appPath, "portapps.cfg"),
+        path.join(appPath, 'portapps.cfg'),
         firefoxCfgContent,
-        (err) => {
+        err => {
           if (err) {
             console.error(err)
-            
           }
         }
       )
@@ -330,23 +331,21 @@ pref('browser.tabs.drawInTitlebar', true)
       global.platform.darwin
     ) {
       fs.writeFile(
-        path.join(prefPath, "autoconfig.js"),
+        path.join(prefPath, 'autoconfig.js'),
         autoconfigContent,
-        (err) => {
+        err => {
           if (err) {
             console.error(err)
-            
           }
         }
       )
 
       fs.writeFile(
-        path.join(appPath, "firefox.cfg"),
+        path.join(appPath, 'firefox.cfg'),
         firefoxCfgContent,
-        (err) => {
+        err => {
           if (err) {
             console.error(err)
-            
           }
         }
       )
@@ -355,28 +354,26 @@ pref('browser.tabs.drawInTitlebar', true)
 
   async isFirefoxRanOnce() {
     const pointPath = await module.exports.getPointPath()
-    return fs.pathExistsSync(
-      path.join(pointPath, this.flagPath)
-    )
+    return fs.pathExistsSync(path.join(pointPath, this.flagPath))
   }
 
   async setFirefoxRanOnce() {
     const pointPath = await module.exports.getPointPath()
-    fs.writeFileSync(path.join(pointPath, this.flagPath), "")
+    fs.writeFileSync(path.join(pointPath, this.flagPath), '')
   }
 
   async getLastVersionFirefox() {
-    const url = "https://product-details.mozilla.org/1.0/firefox_versions.json"
+    const url = 'https://product-details.mozilla.org/1.0/firefox_versions.json'
 
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       https.https.get(url, (res: { on: (arg0: string, arg1: any) => void }) => {
-        let data = ""
+        let data = ''
 
-        res.on("data",  (chunk: string) => {
+        res.on('data', (chunk: string) => {
           data += chunk
-         })
+        })
 
-        res.on("end", () => {
+        res.on('end', () => {
           try {
             const json = JSON.parse(data)
             resolve(json.LATEST_FIREFOX_VERSION)
