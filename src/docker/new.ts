122ddee6e89ce2027091a58c9db00ef3164aa94e
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-02-23 11:47:40 +0530

    Update linux install commands

--- src/docker/new.ts
+++ src/docker/new.ts
@@ -60,15 +60,18 @@ class Docker {
     }
 
     if (global.platform.linux) {
+      this.execSudoCommand(
+        'apt-get remove docker docker-engine docker.io containerd runc'
+      )
       this.execSudoCommand('apt-get update')
       this.execSudoCommand(
-        'apt-get install apt-transport-https ca-certificates curl gnupg lsb-release'
+        'apt-get install ca-certificates curl gnupg lsb-release'
       )
       this.execSudoCommand(
-        'curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
+        'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
       )
       this.execSudoCommand(
-        `echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable' | tee /etc/apt/sources.list.d/docker.list > /dev/null`
+        `echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null`
       )
       this.execSudoCommand('apt-get update')
       this.execSudoCommand(
