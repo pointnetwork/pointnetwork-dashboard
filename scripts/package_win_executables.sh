e94c7f036c85df63509d81c0b94e5ef898559194
Andres Eduardo Rivero Salguedo <aeriverosalguedo@gmail.com>
2022-03-14 19:41:25 -0400

    Script generate win installer msi

--- scripts/package_win_executables.sh
+++ scripts/package_win_executables.sh
@@ -4,8 +4,6 @@
 
 VERSION=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)
 
-DEB_FILE=./out/make/deb/x64/pointnetwork-dashboard_"$VERSION"_amd64.deb
-
 WIN_FILE="./out/make/squirrel.windows/x64/pointnetwork-dashboard-${VERSION} Setup.exe"
 
 mkdir -p ./out/win_executables/pkg
---
+++ scripts/package_win_installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script will rename, add execution permissions and compress point executables.
+
+VERSION=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)
+
+WIN_FILE="./out/pointDashboard_windows_installer/pointnetwork-dashboard.msi"
+
+mkdir -p ./out/win_executables/pkg
+chmod +x "$WIN_FILE"
+mv "$WIN_FILE" ./out/win_executables/pkg/pointnetwork-windows_installer.msi
+7z a ./out/win_executables/pointnetwork-windows_installer.zip ./out/win_executables/pkg/pointnetwork-windows_installer.msi
