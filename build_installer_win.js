3356e118dd08f7d4ef0d6105bbede55af4490551
Andres Rivero <aeriverosalguedo@gmail.com>
2022-03-14 15:17:55 -0400

    script for windws

---
+++ build_installer_win.js
@@ -0,0 +1,38 @@
+ // To generate installer you need to install https://wixtoolset.org/releases/ 
+// ./build_installer.js
+
+// 1. Import Modules
+const { MSICreator } = require('electron-wix-msi');
+const path = require('path');
+
+// 2. Define input and output directory.
+// Important: the directories must be absolute, not relative e.g
+// appDirectory: "C:\\Users\sdkca\Desktop\OurCodeWorld-win32-x64", 
+const APP_DIR = path.resolve(__dirname, './out/pointnetwork-dashboard-win32-x64');
+// outputDirectory: "C:\\Users\sdkca\Desktop\windows_installer", 
+const OUT_DIR = path.resolve(__dirname, './out/pointDashboard_windows_installer');
+
+// 3. Instantiate the MSICreator
+const msiCreator = new MSICreator({
+    appDirectory: APP_DIR,
+    outputDirectory: OUT_DIR,
+
+    // Configure metadata
+    description: 'Point Dashboard',
+    exe: 'pointnetwork-dashboard',
+    name: 'Point Dashboard',
+    manufacturer: 'PointNetwork',
+    version: '1.0.0',
+
+    // Configure installer User Interface
+    ui: {
+        chooseDirectory: true
+    },
+});
+
+// 4. Create a .wxs template file
+msiCreator.create().then(function(){
+
+    // Step 5: Compile the template to a .msi file
+    msiCreator.compile();
+});
