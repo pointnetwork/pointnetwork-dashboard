6d14380aabd7b7c2a4020fb2176e0edd4413421e
Andres Rivero <aeriverosalguedo@gmail.com>
2022-05-03 22:48:30 -0400

    win installer

--- scripts/package_linux_executables.sh
+++ scripts/package_linux_executables.sh
@@ -4,13 +4,13 @@
 
 VERSION=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)
 
-DEB_FILE=./out/make/deb/x64/pointnetwork-dashboard_"$VERSION"_amd64.deb
-RPM_FILE=./out/make/rpm/x64/pointnetwork-dashboard-"$VERSION"-1.x86_64.rpm
+DEB_FILE=./out/make/deb/x64/point_"$VERSION"_amd64.deb
+RPM_FILE=./out/make/rpm/x64/point-"$VERSION"-1.x86_64.rpm
 
 mkdir -p ./out/linux_executables
 
-mv $DEB_FILE ./out/linux_executables/point-dashboard.deb
-mv $RPM_FILE ./out/linux_executables/point-dashboard.rpm
+mv $DEB_FILE ./out/linux_executables/point.deb
+mv $RPM_FILE ./out/linux_executables/point.rpm
 
 for FILE in ./out/linux_executables/*; do
   chmod +x $FILE
@@ -18,5 +18,5 @@ for FILE in ./out/linux_executables/*; do
   EXTENSION=${tmp#*\.};
   mkdir -p ./out/linux_executables/$EXTENSION;
   mv $FILE ./out/linux_executables/$EXTENSION;
-  tar -czvf ./out/linux_executables/point-dashboard-$EXTENSION.tar.gz -C ./out/linux_executables/$EXTENSION/ ./point-dashboard.$EXTENSION;
+  tar -czvf ./out/linux_executables/point-dashboard-$EXTENSION.tar.gz -C ./out/linux_executables/$EXTENSION/ ./point.$EXTENSION;
 done
