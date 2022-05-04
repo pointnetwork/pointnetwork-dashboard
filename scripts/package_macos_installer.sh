5fddb58ac61778b0c345bb9a9e75871f6062b86d
Andres Rivero <aeriverosalguedo@gmail.com>
2022-05-03 23:45:48 -0400

    fix errors

--- scripts/package_macos_installer.sh
+++ scripts/package_macos_installer.sh
@@ -7,4 +7,4 @@ MAC_FILE=./out/point.dmg
 mkdir -p ./out/mac_executables/pkg
 chmod +x $MAC_FILE
 mv $MAC_FILE ./out/mac_executables/pkg/point.dmg
-tar -czvf ./out/mac_executables/point-dmg.tar.gz -C ./out/mac_executables/pkg ./point.dmg
+tar -czvf ./out/mac_executables/point-dashboard-dmg.tar.gz -C ./out/mac_executables/pkg ./point.dmg
