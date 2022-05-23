831fb170736e6926aa845f1aa85bcd2afb076ba5
Andres Eduardo Rivero Salguedo <aeriverosalguedo@gmail.com>
2022-05-22 23:10:32 -0400

    add start up

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,12 @@
 import installer, { Installer } from '../installer'
 import dashboard from '../dashboard'
+import AutoLaunch from 'auto-launch'
 import welcome from '../welcome'
 import helpers from '../../shared/helpers'
- export default async function main() {
+export default async function main() {
 
   helpers.getPlatform()
-  if (!(await Installer.isInstalled()) ) {
+  if (!(await Installer.isInstalled())) {
     installer()
   } else {
     if (! await helpers.isLoggedIn()) {
@@ -15,8 +16,29 @@ import helpers from '../../shared/helpers'
     }
 
   }
- }
+}
 
-  ; (async () => {
-    main()
-  })()
+; (async () => {
+
+  const pointAutoLauncher = new AutoLaunch({
+    name: 'point',
+    path: '/Applications/point.app',
+  });
+
+  pointAutoLauncher.enable();
+
+  // pointAutoLauncher.disable();
+
+
+  pointAutoLauncher.isEnabled()
+    .then(function (isEnabled) {
+      if (isEnabled) {
+        return;
+      }
+      pointAutoLauncher.enable();
+    })
+    .catch(function () {
+      // handle error
+    });
+  main()
+})()
