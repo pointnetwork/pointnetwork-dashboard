6cacfe623a14112f6056d09ec35b70c45d43c834
Amaury Hernandez-Aguila <amherag@gmail.com>
2022-06-02 23:55:09 -0700

    New background image for DMG.

--- scripts/create-mac-intaller.js
+++ scripts/create-mac-intaller.js
@@ -7,6 +7,11 @@ async function buildDMG() {
     out: './out',
     icon: './assets/icon.icns',
     overwrite: true,
+    background: './assets/dmg-background.png',
+    contents: [
+      { x: 520, y: 370, type: 'link', path: '/Applications'},
+      { x: 320, y: 370, type: 'file', path: './out/point-darwin-x64/point.app'}
+    ],
     additionalDMGOptions: {
       "code-sign": {
         "signing-identity": 'Developer ID Application: POINT LABS FZCO (44K963DDUU)'
