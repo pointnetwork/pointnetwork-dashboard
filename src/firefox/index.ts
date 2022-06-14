f9fb3f6b61b595a7cf6fdb92a2df0be0d53572e1
Amaury Hernandez-Aguila <amherag@gmail.com>
2022-06-14 00:32:50 -0700

    Simplified the solution.

--- src/firefox/index.ts
+++ src/firefox/index.ts
@@ -319,15 +319,15 @@ export default class {
   }
 
   async launch() {
-    const processes = await find('name', /firefox*/gi)
-    if (processes.length > 0) {
-      for (const p of processes) {
-        if (!p.bin.match(/point-browser/)) continue
-        logger.info('Firefox already Running')
-        this.window.webContents.send('firefox:active', true)
-        return
-      }
-    }
+    // const processes = await find('name', /firefox*/gi)
+    // if (processes.length > 0) {
+    //   for (const p of processes) {
+    //     if (!p.bin.match(/point-browser/)) continue
+    //     logger.info('Firefox already Running')
+    //     this.window.webContents.send('firefox:active', true)
+    //     return
+    //   }
+    // }
     const cmd = await this.getBinPath()
     const profilePath = path.join(
       helpers.getHomePath(),
@@ -336,7 +336,7 @@ export default class {
 
     let browserCmd = `"${cmd}" --first-startup --profile "${profilePath}" --url https://point`
     if (global.platform.darwin)
-      browserCmd = `open "${cmd}" --args --first-startup --profile "${profilePath}" --url https://point`
+      browserCmd = `open -W "${cmd}" --args --first-startup --profile "${profilePath}" --url https://point`
 
     this.window.webContents.send('firefox:active', true)
     try {
