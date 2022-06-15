5544b39e4a2c845d6803d9f1b02bcba75ae87868
Amaury Hernandez-Aguila <amherag@gmail.com>
2022-06-14 22:37:46 -0700

    Trying out how to zip point.app on GA.

--- scripts/package_macos_executables.sh
+++ scripts/package_macos_executables.sh
@@ -2,9 +2,20 @@
 
 # This script will rename, add execution permissions and compress point executables.
 
-MAC_FILE=./out/pointnetwork-dashboard-darwin-x64/pointnetwork-dashboard.app
+MAC_FILE=./out/point-darwin-x64/point.app
 
-mkdir -p ./out/mac_executables/pkg
+mkdir -p ./out/mac_executables/zip
 chmod +x $MAC_FILE
-mv $MAC_FILE ./out/mac_executables/pkg/point-dashboard.app
-tar -czvf ./out/mac_executables/point-dashboard.tar.gz -C ./out/mac_executables/pkg ./point-dashboard.app
+mv $MAC_FILE ./out/mac_executables/zip/point.app
+# tar -czvf ./out/mac_executables/point-dashboard.tar.gz -C ./out/mac_executables/pkg ./point-dashboard.app
+zip -r -y ./point.zip ./out/mac_executables/zip/point.app
+
+echo '---'
+ls .
+echo '---'
+ls out
+echo '---'
+ls out/mac_executables/
+echo '---'
+ls out/mac_executables/zip
+echo '---'
