ef5bf5fbfa22854d2c9c6a8c6cd3771c35e0c462
Amaury Hernandez-Aguila <amherag@gmail.com>
2022-06-23 01:13:21 -0700

    Cleaning up code.

--- scripts/package_win_installer.sh
+++ scripts/package_win_installer.sh
@@ -8,14 +8,6 @@ WIN_FILE="./out/pointDashboard_windows_installer/point.msi"
 RELEASES_FILE="./out/make/squirrel.windows/x64/RELEASES"
 NUPKG_FILE="./out/make/squirrel.windows/x64/point-$VERSION-full.nupkg"
 
-echo '==='
-pushd ./out/make/squirrel.windows/x64/
-echo $VERSION
-echo '==='
-ls -al
-popd
-echo '==='
-
 mkdir -p ./out/win_executables/pkg
 chmod +x "$WIN_FILE"
 mv "$WIN_FILE" ./out/win_executables/pkg/point.msi
