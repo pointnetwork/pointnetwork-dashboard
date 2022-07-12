cbbdfa02f9660b01d05081129aa4e1383c699727
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-07-12 17:22:50 +0530

    Get the identity info and address from the context

--- src/dashboard/context/MainStatusContext.ts
+++ src/dashboard/context/MainStatusContext.ts
@@ -1,24 +1,35 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from 'react'
 import {
   DashboardChannelsEnum,
   FirefoxChannelsEnum,
   NodeChannelsEnum,
-  UninstallerChannelsEnum
-} from "../../@types/ipc_channels";
-import {LaunchProcessLog} from "../../@types/generic";
-import {MainStatus} from "../../@types/context";
+  UninstallerChannelsEnum,
+} from '../../@types/ipc_channels'
+import { IdentityLog, LaunchProcessLog } from '../../@types/generic'
+import { MainStatus } from '../../@types/context'
 
 export const useMainStatus = () => {
+  // General
   const [identifier, setIdentifier] = useState<string>('')
-  const [browserVersion, setBrowserVersion] = useState<string>('')
-  const [nodeVersion, setNodeVersion] = useState<string>('')
-  const [launchAttempts, setLaunchAttempts] = useState<number>(0)
   const [loader, setIsLaunching] = useState<{
     isLoading: boolean
     message: string
   }>({ isLoading: true, message: 'Starting Point Network' })
-  const [isBrowserRunning, setIsBrowserRunning] = useState<boolean>(false)
+  const [launchAttempts, setLaunchAttempts] = useState<number>(0)
+  // Node
+  const [nodeVersion, setNodeVersion] = useState<string>('')
   const [isNodeRunning, setIsNodeRunning] = useState<boolean>(false)
+  // Browser
+  const [browserVersion, setBrowserVersion] = useState<string>('')
+  const [isBrowserRunning, setIsBrowserRunning] = useState<boolean>(false)
+  // Identity
+  const [identityInfo, setIdentityInfo] = useState<IdentityLog>({
+    identity: '',
+    isFetching: true,
+    address: '',
+    log: '',
+  })
+  const [balance, setBalance] = useState<number | string>(0)
 
   // Register these events once to prevent leaks
   const setListeners = () => {
@@ -65,13 +76,25 @@ export const useMainStatus = () => {
         message: 'Logging Out',
       })
     })
+
+    window.Dashboard.on(NodeChannelsEnum.get_identity, (_: string) => {
+      const parsed: IdentityLog = JSON.parse(_)
+      setIdentityInfo(parsed)
+    })
+
+    window.Dashboard.on(
+      DashboardChannelsEnum.check_balance_and_airdrop,
+      (_: string) => {
+        setBalance(_)
+      }
+    )
   }
 
   const getInfo = async () => {
     const [id, pointNodeVersion, firefoxVersion] = await Promise.all([
       window.Dashboard.getIndentifier(),
       window.Dashboard.getNodeVersion(),
-      window.Dashboard.getFirefoxVersion()
+      window.Dashboard.getFirefoxVersion(),
     ])
     setIdentifier(id)
     setNodeVersion(pointNodeVersion)
@@ -91,7 +114,12 @@ export const useMainStatus = () => {
 
   // 2. Once node is running, we launch the browser
   useEffect(() => {
-    if (isNodeRunning) window.Dashboard.launchBrowser()
+    if (isNodeRunning) {
+      window.Dashboard.launchBrowser()
+      window.Dashboard.getIdentityInfo()
+      window.Dashboard.sendGeneratedEventToBounty()
+      window.Dashboard.checkBalanceAndAirdrop()
+    }
   }, [isNodeRunning])
 
   // 3. Once browser is running, we finish the launch procedure
@@ -111,8 +139,12 @@ export const useMainStatus = () => {
     browserVersion,
     nodeVersion,
     launchAttempts,
-    loader
+    loader,
+    identityInfo,
+    balance,
   }
 }
 
-export const MainStatusContext = createContext<MainStatus>({} as unknown as MainStatus)
+export const MainStatusContext = createContext<MainStatus>(
+  {} as unknown as MainStatus
+)
