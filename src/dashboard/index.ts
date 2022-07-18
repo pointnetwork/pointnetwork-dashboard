929e2097a848dd31353953de1d27afddba0ceda5
Koochr <koochr@protonmail.com>
2022-07-18 16:44:36 +0100

    Fix a couple more of forgotten awaits

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -346,9 +346,9 @@ export default async function () {
     {
       channel: GenericChannelsEnum.open_external_link,
       // @ts-ignore
-      listener(_, link: string) {
+      async listener(_, link: string) {
         try {
-          shell.openExternal(link)
+          await shell.openExternal(link)
         } catch (error) {
           logger.error(ErrorsEnum.DASHBOARD_ERROR, error)
         }
