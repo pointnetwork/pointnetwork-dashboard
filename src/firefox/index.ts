c25cc3aa4035d7c302ad799f745eca253f2e1988
Amaury Hernandez-Aguila <amherag@gmail.com>
2022-08-18 07:04:23 -0700

    Using `open` for opening browser on macOS.

--- src/firefox/index.ts
+++ src/firefox/index.ts
@@ -191,7 +191,7 @@ class Firefox {
                 '.point/keystore/liveprofile'
             );
 
-            const cmd = global.platform.darwin ? `${binFile}/Contents/MacOS/firefox` : binFile;
+            const cmd = global.platform.darwin ? `open -W ${binFile}/Contents/MacOS/firefox` : binFile;
             this.logger.info('Launching');
             const proc = spawn(
                 cmd,
