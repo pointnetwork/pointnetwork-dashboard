b120e9317f0bb1fab1a4f5026dbea6dd2c45994f
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-09-01 22:09:55 +0530

    Dont check for referral code and default to 0 value

--- src/bounty/index.ts
+++ src/bounty/index.ts
@@ -20,18 +20,18 @@ class Bounty {
     referralCode = '000000000000';
     isInstalledEventSent = false;
 
-    constructor({window}: { window: BrowserWindow }) {
+    constructor({window}: {window: BrowserWindow}) {
         this.window = window;
         this.logger = new Logger({window, module: 'bounty'});
     }
 
-    async init () {
-        await this._getReferralCode();
+    async init() {
+        // await this._getReferralCode();
     }
 
     /**
-   * Sends the saved referral code to bounty server with `event=generated`
-   */
+     * Sends the saved referral code to bounty server with `event=generated`
+     */
     async sendGenerated() {
         try {
             const fileContents = JSON.parse(
@@ -41,9 +41,7 @@ class Bounty {
             );
             const referralCode = fileContents.referralCode;
 
-            const addressRes = await axios.get(
-                'http://localhost:2468/v1/api/wallet/address'
-            );
+            const addressRes = await axios.get('http://localhost:2468/v1/api/wallet/address');
             const address = addressRes.data.data.address;
 
             if (!fileContents.isGeneratedEventSent && address) {
@@ -51,9 +49,7 @@ class Bounty {
                 await axios.get(
                     `https://bounty.pointnetwork.io/ref_success?event=generated&ref=${referralCode}&addr=${address}`
                 );
-                this.logger.info(
-                    'Sent event=generated to https://bounty.pointnetwork.io'
-                );
+                this.logger.info('Sent event=generated to https://bounty.pointnetwork.io');
                 await fs.writeFile(
                     path.join(helpers.getPointPath(), 'infoReferral.json'),
                     JSON.stringify({
@@ -73,8 +69,8 @@ class Bounty {
     }
 
     /**
-   * Sends the referral code to bounty server with `event=install`
-   */
+     * Sends the referral code to bounty server with `event=install`
+     */
     async sendInstalled(): Promise<void> {
         try {
             this.logger.info('Sending "event=install" to bounty server');
@@ -94,17 +90,15 @@ class Bounty {
     }
 
     /**
-   * Sends the referral code to bounty server with `event=install_started`
-   */
+     * Sends the referral code to bounty server with `event=install_started`
+     */
     async sendInstallStarted(): Promise<void> {
         try {
             this.logger.info('Sending "event=install_started" to bounty server');
             await axios.get(
                 `https://bounty.pointnetwork.io/ref_success?event=install_started&ref=${this.referralCode}&addr=0x0000000000000000000000000000000000000000`
             );
-            this.logger.info(
-                'Sent event=install_started to https://bounty.pointnetwork.io'
-            );
+            this.logger.info('Sent event=install_started to https://bounty.pointnetwork.io');
         } catch (error) {
             this.logger.error({
                 errorType: ErrorsEnum.BOUNTY_ERROR,
@@ -115,8 +109,8 @@ class Bounty {
     }
 
     /**
-   * Saves that referral code in ~/.point/infoReferral.json
-   */
+     * Saves that referral code in ~/.point/infoReferral.json
+     */
     async _saveReferralInfo() {
         try {
             this.logger.info('Saving referralCode to "infoReferral.json"');
@@ -138,10 +132,10 @@ class Bounty {
     }
 
     /**
-   * Reads various directories and sets the referralCode
-   */
+     * Reads various directories and sets the referralCode
+     */
     async _getReferralCode() {
-    // Get referral code from the trash folder
+        // Get referral code from the trash folder
         let trashDir;
         let trashDirContent: string[] = [];
 
@@ -194,19 +188,15 @@ class Bounty {
         }
 
         // Make sure it's one of our file downloads and pick the first one
-        const matchDir = [
-            ...downloadDirContent,
-            ...desktopDirContent,
-            ...trashDirContent
-        ]
+        const matchDir = [...downloadDirContent, ...desktopDirContent, ...trashDirContent]
             .filter(
                 (dir: string) =>
                     dir.includes('point-') &&
-          dir.match(/-\d{12}\./) &&
-          (dir.includes('Linux-Debian-Ubuntu') ||
-            dir.includes('Linux-RPM-Centos-Fedora') ||
-            dir.includes('MacOS-installer') ||
-            dir.includes('Windows-installer'))
+                    dir.match(/-\d{12}\./) &&
+                    (dir.includes('Linux-Debian-Ubuntu') ||
+                        dir.includes('Linux-RPM-Centos-Fedora') ||
+                        dir.includes('MacOS-installer') ||
+                        dir.includes('Windows-installer'))
             )
             .map((dir: string) => path.join(helpers.getHomePath(), dir))[0];
 
@@ -223,10 +213,9 @@ class Bounty {
             // Get the referral code
             this.referralCode = requiredDir.slice(requiredDir.length - 12);
         }
-        if (
-            Number.isNaN(Number(this.referralCode)) ||
-      Number(this.referralCode) < 0
-        ) {this.referralCode = '000000000000';}
+        if (Number.isNaN(Number(this.referralCode)) || Number(this.referralCode) < 0) {
+            this.referralCode = '000000000000';
+        }
 
         this.logger.info('Referral Code: ', this.referralCode);
     }
